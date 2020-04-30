7224b4fe4dc270cba2138fd2bcf63804632fead2
MykytaBAshenko <nikita.bashenko2001@gmail.com>
2020-05-01 00:45:20 +0300

    Updates

---
+++ precache-manifest.c714e6bbcd243f0ec7a858418be51d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9613e2098a80cce3aa12c939720f2c",
+    "url": "/trash/build/index.html"
+  },
+  {
+    "revision": "1a0a6da5f17ce86a3ed0",
+    "url": "/trash/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b67d201d04e2a878e91f",
+    "url": "/trash/build/static/js/2.fc73e283.chunk.js"
+  },
+  {
+    "revision": "cfcdebbd5c361eb80dad4bc286f1bc4f",
+    "url": "/trash/build/static/js/2.fc73e283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0a6da5f17ce86a3ed0",
+    "url": "/trash/build/static/js/main.f7e7c199.chunk.js"
+  },
+  {
+    "revision": "c57c239e935fb0148945",
+    "url": "/trash/build/static/js/runtime-main.2dd98f39.js"
+  }
+]);
